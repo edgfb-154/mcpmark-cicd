b62587ba0c0f081f41b992cb7f51e7bb9047942b
edgfb-154 <wlu4hj8q@154040.xyz>
2026-01-16 12:23:04 +0800

    Fix linting errors in example.js

--- src/example.js
+++ src/example.js
@@ -1,6 +1,5 @@
-const unusedVariable = "This is a test";
-console.log("Hello, world!");
+console.log('Hello, world!');
 function testFunction() {
   let value = 10;
-  return value
+  return value;
 }
